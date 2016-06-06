7ad51f9f5cc4b45b06253bb8d89b3293e1651d29
Jose Luis Salas <josacar@users.noreply.github.com>
2016-06-06 14:42:45 +0000

    Move Cf::Client to a separate file

--- lib/cf.rb
+++ lib/cf.rb
@@ -1,39 +1,9 @@
 require 'public_suffix'
 require 'json'
 require 'httpclient'
-
-# Test Cf::Client unitary
+require 'cf/client'
 
 module Cf
-  class Client
-    BASE_URL = 'https://api.cloudflare.com/client/v4'.freeze
-
-    def initialize(email, auth_key)
-      @headers = {
-        'Content-Type' => 'application/json',
-        'X-Auth-Email' => email,
-        'X-Auth-Key' => auth_key
-      }
-    end
-
-    def get(query, params)
-      http = HTTPClient.new
-      http.get_content(BASE_URL + query, params, @headers)
-    end
-
-    def post(query, data)
-      http = HTTPClient.new
-      json_data = JSON.generate(data)
-      http.post(BASE_URL + query, json_data, @headers)
-    end
-
-    def put(query, data)
-      http = HTTPClient.new
-      json_data = JSON.generate(data)
-      http.put(BASE_URL + query, json_data, @headers)
-    end
-  end
-
   class Credentials
     def initialize(email, auth_key)
       @email = email
--- spec/resolve_domain_spec.rb
+++ spec/resolve_domain_spec.rb
@@ -2,9 +2,30 @@ require 'cf'
 
 describe 'Resolve domain' do
   context 'when a domain is registered' do
-    let(:domain) { 'flywire.cc' }
+    let(:domain) { 'cname.flywire.cc' }
     let(:destination) { 'peertransfer.me' }
     let(:type) { 'CNAME' }
+    before do
+      register_domain(domain, destination)
+    end
+
+    it 'resolves to right destination' do
+      expect(resolve(domain)).to eq(destination)
+    end
+
+    def register_domain(domain, destination)
+      Cf.register(domain, destination, type)
+    end
+
+    def resolve(domain)
+      Cf.resolve(domain)
+    end
+  end
+
+  context 'when a domain contains more than one destination' do
+    let(:domain) { 'a.flywire.cc' }
+    let(:destination) { ['1.2.3.4', '5.6.7.8'] }
+    let(:type) { 'A' }
     before do
       register_domain(domain, destination)
     end
